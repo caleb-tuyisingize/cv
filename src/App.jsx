d5d2ff8db5dbed140fb812eb85f89ab2fed034d2
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-08-16 20:35:23 +0200

    The landing of my CV by hidding the landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
+
 import Header from "./components/Header.jsx"
 import Landing from "./components/Landing.jsx"
+
+
 function App() {
+  
+const [isHidden, setIsHidden] = useState(false);
+
+const hiddingFn = (label)=>{
+  console.log(`${label} clicked`)
+setIsHidden(true);
+}
 
   return (
     <>
       <div className="bg-gray-100 h-[calc(98vh-5rem)] rounded-b-[30px] mx-auto w-[90%] mt-10">
         <h1 className="text-4xl text-center font-bold shadow-xl h-20">My CV</h1>
         <div className="flex lg:flex-row">
-          <Header/>
-          <Landing/>
+          <Header hiddingFn={hiddingFn}/>
+          {!isHidden && <Landing/>}
         </div>
       </div>
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,30 @@ import BadgeIcon from '@mui/icons-material/Badge';
 import SchoolIcon from '@mui/icons-material/School';
 import SyncAltIcon from '@mui/icons-material/SyncAlt';
 import PermPhoneMsgIcon from '@mui/icons-material/PermPhoneMsg';
-export default function Header() {
+
+
+export default function Header({ hiddingFn }) {
+ const menus = [
+    { icon: <PortraitIcon />, label: 'Caleb Tuyisingize' },
+    { icon: <BadgeIcon />, label: 'Work Experience' },
+    { icon: <SchoolIcon />, label: 'Education' },
+    { icon: <SyncAltIcon />, label: 'Skills' },
+    { icon: <PermPhoneMsgIcon />, label: 'Contact' },
+  ];
+
   return (
     <header className="sticky left-0 z-10 w-20 h-[calc(91vh-6rem)] rounded-bl-[30px] text-8xl bg-gray-800 text-white lg:flex lg:space-x-10 p-4 text-center">
-      <div className="profile scale-x-[2] scale-y-[2]"><PortraitIcon /><br /><span className='hidden lg:flex'>Caleb Tuyisingize</span></div>
-      <div className="profile scale-x-[2] scale-y-[2]"><BadgeIcon /><br /><span className='hidden lg:flex'>Work Experience</span></div>
-      <div className="profile scale-x-[2] scale-y-[2]"><SchoolIcon /><br /><span className='hidden lg:flex'>Education</span></div>
-      <div className="profile scale-x-[2] scale-y-[2]"><SyncAltIcon /><br /><span className='hidden lg:flex'>Skills</span></div>
-      <div className="profile scale-x-[2] scale-y-[2]"><PermPhoneMsgIcon /><br /><span className='hidden lg:flex'>Contact</span></div>
+      {menus.map((menu, i) => (
+        <div
+          key={i}
+          className="profile scale-x-[2] scale-y-[2] cursor-pointer"
+          onClick={() => hiddingFn(menu.label)}
+        >
+          {menu.icon}
+          <br />
+          <span className="hidden lg:flex">{menu.label}</span>
+        </div>
+      ))}
     </header>
   );
 }
