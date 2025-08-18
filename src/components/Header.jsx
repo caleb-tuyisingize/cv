d5f67e03563432bf6c31792f094a8e9a5669fa83
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-08-18 18:05:58 +0200

    The final phone view cv

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,32 +3,42 @@ import BadgeIcon from '@mui/icons-material/Badge';
 import SchoolIcon from '@mui/icons-material/School';
 import SyncAltIcon from '@mui/icons-material/SyncAlt';
 import PermPhoneMsgIcon from '@mui/icons-material/PermPhoneMsg';
+import BedroomBabyIcon from '@mui/icons-material/BedroomBaby';
 import {Link} from 'react-router-dom';
-import Experience from "../pages/experience.jsx";
+
 
 export default function Header({ hiddingFn }) {
  const menus = [
-    { icon: <PortraitIcon />, label: 'Caleb Tuyisingize', path: '/about' },
-    { icon: <BadgeIcon />, label: 'Work Experience', path: '/Experience' },
-    { icon: <SchoolIcon />, label: 'Education', path: '/education' },
-    { icon: <SyncAltIcon />, label: 'Skills', path: '/skills' },
-    { icon: <PermPhoneMsgIcon />, label: 'Contact', path: '/contact' },
+   { icon: <PortraitIcon />, label: 'Caleb Tuyisingize', path: '/About' },
+   { icon: <BadgeIcon />, label: 'Work Experience', path: '/Experience' },
+   { icon: <PermPhoneMsgIcon />, label: 'Contact', path: '/About' },
+   { icon: <SchoolIcon />, label: 'Skills', path: '/Contact' },
+   { icon: <SyncAltIcon />, label: 'Education', path: '/Education' },
+   { icon: null, label: 'Skills', path: '/Skills' },
   ];
 
   return (
     <header className="sticky left-0 z-10 w-20 h-[calc(91vh-6rem)] rounded-bl-[30px] text-8xl bg-gray-800 text-white lg:flex lg:space-x-10 p-4 text-center">
       {menus.map((menu, i) => (
-        <Link to={menu.path} className="flex flex-col items-center text-gray-300 hover:text-white transition-colors duration-200">
-        <div 
+              <Link
           key={i}
+          to={menu.path}
           className="profile scale-x-[2] scale-y-[2] cursor-pointer"
           onClick={() => hiddingFn(menu.label)}
         >
+       
+        <div 
+          key={i}
+          className="profile scale-x-[2] scale-y-[2] cursor-pointer"
+           onClick={() => hiddingFn(menu.label)}
+           >
           {menu.icon}
+
           <br />
           <span className="hidden lg:flex">{menu.label}</span>
         </div>
-      </Link>
+          </Link>
+     
       ))
     }
     </header>
